d8a6d6533cd1b174e8474fb8b4244dab1bb01d5f
Jumery <18824188375@163.com>
2017-06-22 14:50:18 +0800

    tv

---
+++ app/src/main/java/com/example/liang/cg570sdk/ThridActivity.java
@@ -0,0 +1,19 @@
+package com.example.liang.cg570sdk;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liang on 2017/6/22.
+ */
+
+public class ThridActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/example/liang/cg570sdk/Utils.java
@@ -0,0 +1,8 @@
+package com.example.liang.cg570sdk;
+
+/**
+ * Created by liang on 2017/6/22.
+ */
+
+public class Utils {
+}
