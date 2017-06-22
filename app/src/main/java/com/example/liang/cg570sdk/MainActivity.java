ebedf0ff79ae4b1ddaa59ca6ed8d37f3775e8467
Jumery <18824188375@163.com>
2017-06-22 14:31:04 +0800

    tv

--- app/src/main/java/com/example/liang/cg570sdk/MainActivity.java
+++ app/src/main/java/com/example/liang/cg570sdk/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.liang.cg570sdk;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = new TextView(this);
+        tv.setText("adsafksdfksndfnsdf");
+        setContentView(tv);
     }
 }
