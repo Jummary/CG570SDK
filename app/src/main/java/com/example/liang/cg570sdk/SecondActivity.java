4135eb049d58f42c1b09f736a1f48c3ec09f7a83
Jumery <18824188375@163.com>
2017-06-22 14:19:28 +0800

    second

---
+++ app/src/main/java/com/example/liang/cg570sdk/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.liang.cg570sdk;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liang on 2017/6/22.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
